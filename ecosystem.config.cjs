bddb3aebfa837825f3416ab54dc0474b056d0515
Pris <cc.circuit.crew@gmail.com>
2025-12-15 16:20:45 +0100

    init multibot base

---
+++ ecosystem.config.cjs
@@ -0,0 +1,74 @@
+module.exports = {
+  apps: [
+    {
+      name: "bot01",
+      cwd: "./bots/bot01",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot02",
+      cwd: "./bots/bot02",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot03",
+      cwd: "./bots/bot03",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot04",
+      cwd: "./bots/bot04",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot05",
+      cwd: "./bots/bot05",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot06",
+      cwd: "./bots/bot06",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot07",
+      cwd: "./bots/bot07",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot08",
+      cwd: "./bots/bot08",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot09",
+      cwd: "./bots/bot09",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    },
+    {
+      name: "bot10",
+      cwd: "./bots/bot10",
+      script: "index.js",
+      autorestart: true,
+      max_restarts: 10
+    }
+  ]
+};
